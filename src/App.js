5f15b46b5c212f318fb174c796f289eec5120df1
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-04-01 14:11:42 +0600

    add file FUlAPst

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
+
 import { useState } from 'react';
-import FullPost from './componants/FullPost/FullPost';
-import Posts from './components/Posts/Posts';
-
-// import Albums from"./componants/Albums/Albums";
-// import Photos from"./componants/Photos/Photos";
-// import Todos from"./componants/Todos/Todos";
-// import Users from"./componants/Users/Users";
 import './App.css';
+import FullPost from './componants/FullPost/FullPost';
+import Posts from './componants/Posts/Posts';
 
+function App() {
+  const [selectedPost, setSelectedPost] = useState(0);
 
-function App(){
-  const[selectedPost,setSelectedPost] =useState[0]
   return (
     <div className="App">
- <FullPost id={selectedPost} />
+      <FullPost id={selectedPost} />
       <Posts setSelectedPost={setSelectedPost} />
-  {/* <hr></hr>
-  <Comments/> 
-  <hr></hr>
-  <Albums/> 
-  <hr></hr>
-  <Photos/>
-   <hr></hr>
-  <Todos/>
-   <hr></hr>
- < Users/> */}
     </div>
   );
-  }
+}
 
 export default App;
+
